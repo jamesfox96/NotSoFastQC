4d028e6622b1f5f88371970b0d077ef6f711616d
James <james-fox@mail.com>
2020-10-29 09:13:40 +0000

    Argparse working

--- NotSoFastQC/__main__.py
+++ NotSoFastQC/__main__.py
@@ -4,24 +4,12 @@ import argparse
 def get_options():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-I', required=False, help="input FastQC file path")
+    parser.add_argument('-J', required=False, help="input FastQC file path")
     parser.add_argument('-O', required=False, help="output directory path")
     parser.add_argument('--all', required=False, action='store_true',
                         help="Generates reports for all QC report modules")
-    parser.add_argument('-M', required=False,
-                        help='Input the QC report modules of your choice, separated by a comma \',\':'
-                             '\r\t1 - Per base sequence quality score'
-                             '\r\t2 - Per tile sequence quality'
-                             '\r\t3 - Per sequence quality scores'
-                             '\r\t4 - Per base sequence content'
-                             '\r\t5 - Per sequence GC content'
-                             '\r\t6 - Per base N content'
-                             '\r\t7 - Sequence Length Distribution'
-                             '\r\t8 - Sequence Duplication Levels'
-                             '\r\t9 - Overrepresented sequences'
-                             '\r\t10 - Adapter Content'
-                             '\r\t11 - K-mer Content'
-                        )
+    parser.add_argument('-M', required=False, nargs='*', default=[],
+                        help='Input the QC report modules of your choice separated by a space')
     return parser.parse_args()
 
 
@@ -29,6 +17,16 @@ def main():
     print("This is NotSoFastQC!")
 
     args = get_options()
+
+    if args.all is False:
+        print("all = False")
+        for arg in args.M:
+            print(arg)
+    else:
+        print("all == True")
+        args.M = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        for arg in args.M:
+            print(arg)
 
 
 if __name__ == '__main__':
